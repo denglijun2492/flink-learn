223f22051018a77852b872a57717e7ee41f3f7a2
denglijun2492 <183827106@qq.com>
2019-04-07 15:26:29 +0800

    add file

--- wiki-edits/src/main/java/wikiedits/MyWikipediaAnalysis.java
+++ wiki-edits/src/main/java/wikiedits/MyWikipediaAnalysis.java
@@ -1,6 +1,8 @@
 package wikiedits;
 
 import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -8,6 +10,7 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
 
@@ -31,7 +34,7 @@ public class MyWikipediaAnalysis {
 
         //4.每10秒按用户统计编辑的字节数
         DataStream<Tuple2<String, Long>> result = keyedStream
-                .timeWindow(Time.seconds(10))
+                .timeWindow(Time.seconds(5))
                 .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
                     @Override
                     public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) throws Exception {
@@ -42,7 +45,14 @@ public class MyWikipediaAnalysis {
                 });
 
         //打印到控制台
-        result.print();
+//        result.print();
+
+        result.map(new MapFunction<Tuple2<String,Long>, String>() {
+            @Override
+            public String map(Tuple2<String, Long> value) throws Exception {
+                return value.toString();
+            }
+        }).addSink(new FlinkKafkaProducer<>("192.168.1.11:9092","wiki-result", new SimpleStringSchema()));
 
         //执行
         see.execute();
